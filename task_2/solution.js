a12ca62c429a2ee95e11716757661f2aeb65048e
Sergey Korolev <1982ksv@mail.ru>
2021-12-07 18:22:45 +0300

    test5

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,7 +7,7 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum; // создайте переменную shippingSum
 
-    if (productsSum = 0){// если productsSum равно 0,
+    if (productsSum == 0){// если productsSum равно 0,
         shippingSum = 0; // то shippingSum присвоить значение 0
     }
     else if (productsSum >= freeShippingMinSum){
